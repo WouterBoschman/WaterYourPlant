7fd609202dc4be5639f7fd0177b783e26a6b9a3a
WouterBoschman <Wouter.Boschman@gmail.com>
2020-07-07 09:51:14 +0200

    WaterYourPlant

---
+++ wt/src/test/java/nl/WaterYourPlant/wt/WtApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.WaterYourPlant.wt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
